a5c02f45c1dc76869e62d35ecd1fc9b473e9a8e0
sanju50201 <sanjithk50201@gmail.com>
2022-01-16 18:20:54 +0530

    Modified

--- StackExample.java
+++ StackExample.java
@@ -1,50 +1,39 @@
 package practicepackage;
 
-class MyStack
-{
+class MyStack{
 	int MAX=20;		
 	int top=-1;
 	int data[]=new int[MAX];
 		
-	public void push(int d)
-	{
+	public void push(int d){
 		data[top+1]=d;
 		top=top+1;
 	}
 	
-	public int peek()
-	{
+	public int peek(){
 		return data[top+1];
 	}
 	
-	public int pop()
-	{
+	public int pop(){
 		int temp=data[top];
 		top=top-1;
 		return temp;
 	}
-	
-	public void printValues()
-	{
+	public void printValues(){
 		for(int i=0;i<=top;i++)
 			System.out.println(data[i]);
 	}
 	
-	public boolean isEmpty()
-	{
+	public boolean isEmpty(){
 		return (top==-1);	
 	}
 	
-	public int size()
-	{
+	public int size(){
 		return top+1;
 	}
-	
-	
 }
 
-public class StackExample 
-{
+public class StackExample {
 	public static void main(String[] args) 
 	{
 		MyStack exampleStack = new MyStack();		
